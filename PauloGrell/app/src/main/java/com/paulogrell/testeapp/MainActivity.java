498771190b46b4ba33dae49fb98968fe6faae0d2
Paulo Grell <paulogrell@gmail.com>
2016-02-19 23:43:09 -0200

    :)

--- PauloGrell/app/src/main/java/com/paulogrell/testeapp/MainActivity.java
+++ PauloGrell/app/src/main/java/com/paulogrell/testeapp/MainActivity.java
@@ -1,37 +1,39 @@
 package com.paulogrell.testeapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -41,12 +43,54 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            // Criando uma array falsa de strings com os títulos das nossas Aulas
+            String[] data = {
+                    "Aula 1: Getting Start!",
+                    "Aula 2: Como instalar o Android Studio?",
+                    "Aula 3: Como criar um projeto no Android Studio?",
+                    "Aula 4: O que é Activity e Fragment?",
+                    "Aula 5: Como criar a User Interface (UI) do meu Aplicativo Android?"
+            };
+
+            // Criando uma lista (ArrayList) com os dados criados acima
+            List<String> listOfLastPosts = new ArrayList<String>(Arrays.asList(data));
+
+            // Agora que já temos os dados, vamos criar um Adapter, no caso um ArrayAdapter
+
+            ArrayAdapter<String> listOfLastPostsAdapter = new ArrayAdapter<String>(
+                    getActivity(), // O contexto atual
+                    R.layout.list_item_last_posts, // O arquivo de layout de cada item
+                    R.id.list_item_post_title_textview, // O ID do campo a ser preenchido
+                    listOfLastPosts // A fonte dos dados
+            );
+
+            // Inflamos o layout principal
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            // Cria uma referência para a ListView
+            ListView listView = (ListView) rootView.findViewById(R.id.list_last_posts);
+            listView.setAdapter(listOfLastPostsAdapter);
+
+            // Retornamos tudo
+            return rootView;
+        }
     }
 }
